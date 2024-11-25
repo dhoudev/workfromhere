053f550720fc90929572918d18e227fddcecba80
David Houston <davidaehouston@gmail.com>
2024-11-25 18:45:07 +0000

    Adding Global exception handlers

---
+++ backend/src/main/java/dh/dev/workfromhere/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package dh.dev.workfromhere.exceptions;
+
+import dh.dev.workfromhere.spaces.exception.SpaceResourceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SpaceResourceException.class)
+    public ResponseEntity<ErrorDetails> spaceResourceException(SpaceResourceException e) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .errorCode(e.getErrorCode())
+                .message(e.getMessage())
+                .httpStatus(e.getErrorCode().getHttpStatus())
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ backend/src/main/java/dh/dev/workfromhere/spaces/exception/SpaceResourceException.java
@@ -0,0 +1,16 @@
+package dh.dev.workfromhere.spaces.exception;
+
+import dh.dev.workfromhere.exceptions.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class SpaceResourceException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    public SpaceResourceException(ErrorCode errorCode) {
+        super();
+        this.errorCode = errorCode;
+    }
+}
