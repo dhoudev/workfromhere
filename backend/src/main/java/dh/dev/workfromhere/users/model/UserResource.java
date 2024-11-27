bb11aeee5d0d90ec27e260d02562039c05fdd631
David Houston <davidaehouston@gmail.com>
2024-11-27 18:58:29 +0000

    Adding entity detsils for User

--- backend/src/main/java/dh/dev/workfromhere/users/model/UserResource.java
+++ backend/src/main/java/dh/dev/workfromhere/users/model/UserResource.java
@@ -1,12 +1,22 @@
 package dh.dev.workfromhere.users.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class UserResource {
 
+    @Id
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
 }
